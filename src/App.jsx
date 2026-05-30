f4e3fc10f2b30b9fd73b6fe7f06b8149d3bdb7ad
sakthimurugesanr2 <ss1725758@gmail.com>
2026-05-30 21:11:20 +0530

    under-construction

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UnderConstruction from './component/Underconstruction.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <UnderConstruction/>
+    </div>
+  )
+}
+
+export default App
